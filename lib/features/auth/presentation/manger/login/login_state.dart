9cb55bdb8a0723ce9dae5edbddd1b49e49b6ea11
mohamed badr <mahomedbadr05@gmail.com>
2024-05-29 00:34:59 +0300

    finish login cubit state

--- lib/features/auth/presentation/manger/login/login_state.dart
+++ lib/features/auth/presentation/manger/login/login_state.dart
@@ -2,9 +2,19 @@ part of 'login_cubit.dart';
 
 abstract class LoginState extends Equatable {
   const LoginState();
-}
-
-class LoginInitial extends LoginState {
   @override
   List<Object> get props => [];
+}
+
+class LoginInitial extends LoginState {}
+class LoginLoading extends LoginState {}
+class LoginSuccess extends LoginState {
+
+}
+class LoginFailure extends LoginState {
+  final String errorMessage;
+
+  const LoginFailure({
+    required this.errorMessage,
+  });
 }
